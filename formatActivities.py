7493d1368b8edf4f8c891ec8f08e6bc5c41d78d4
Spencer Ferguson-Dryden <sfergusond@gmail.com>
2023-01-11 19:36:26 -0700

    relaxed activity type constraint

--- formatActivities.py
+++ formatActivities.py
@@ -1,18 +1,17 @@
 def formatActivities(activities):
   formattedActivities = []
   for a in activities:
-    if 'Run' in a['type'] or a['type'] in ['Walk', 'Hike']:
-      try:
-        formattedActivity = formatActivity(a)
-        formattedActivities.append(formattedActivity)
-      except Exception as e:
-        print('Format Error:', e)
+    try:
+      formattedActivity = formatActivity(a)
+      formattedActivities.append(formattedActivity)
+    except Exception as e:
+      print('Format Error:', e)
   return formattedActivities
 
 def formatActivity(activity):
   formattedActivity = {}
   formattedActivity['distance'] = int(round(
-    activity['distance'])) if activity.get('distance') else None
+    activity['distance'])) if activity.get('distance') else 0
   formattedActivity['elevation'] = int(round(
     activity['total_elevation_gain'])) if activity.get('total_elevation_gain') else None
   formattedActivity['averageHr'] = round(
@@ -21,4 +20,5 @@ def formatActivity(activity):
   formattedActivity['time'] = activity.get('moving_time', 0)
   formattedActivity['title'] = activity['name'][:500].replace('\'', ' ').replace('\"', ' ')
   formattedActivity['stravaId'] = activity['id']
+  formattedActivity['type'] = activity['type']
   return formattedActivity
